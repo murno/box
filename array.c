b0a5e2300cc7f132c977bd450c6b4d91fb5de6c8
Aleksandr Ogorodnikov <ogorodnikov.a.v@yandex.ru>
2026-04-28 16:52:37 +0300

    refactor: add proper error handling

--- array.c
+++ array.c
@@ -1,6 +1,8 @@
 #include "array.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
 
 struct box_array {
 	int *elements;
@@ -14,6 +16,11 @@ box_array_new(void)
 	BoxArray *a;
 
 	a = malloc((sizeof *a));
+	if (a == NULL) {
+		perror("box_array_new: malloc failed");
+		abort();
+	}
+
 	a->elements = NULL;
 	a->count = 0;
 	a->capacity = 0;
@@ -24,16 +31,24 @@ box_array_new(void)
 void
 box_array_delete(BoxArray *a)
 {
-	free(a->elements);
-	free(a);
+	if (a != NULL) {
+		free(a->elements);
+		free(a);
+	}
 }
 
 void
 box_array_push(BoxArray *a, int e)
 {
+	assert(a != NULL);
+
 	if (a->count == a->capacity) {
 		a->capacity = (a->capacity == 0) ? 1 : a->capacity * 2;
 		a->elements = realloc(a->elements, a->capacity * (sizeof *a->elements));
+		if (a->elements == NULL) {
+			perror("box_array_push: realloc failed");
+			abort();
+		}
 	}
 
 	a->elements[a->count] = e;
@@ -43,23 +58,34 @@ box_array_push(BoxArray *a, int e)
 void
 box_array_pop(BoxArray *a)
 {
+	assert(a != NULL);
+	assert(a->count > 0);
+
 	a->count--;
 }
 
 int
 box_array_at(BoxArray *a, size_t i)
 {
+	assert(a != NULL);
+	assert(i < a->count);
+
 	return a->elements[i];
 }
 
 void
 box_array_set(BoxArray *a, size_t i, int e)
 {
+	assert(a != NULL);
+	assert(i < a->count);
+
 	a->elements[i] = e;
 }
 
 size_t
 box_array_size(BoxArray *a)
 {
+	assert(a != NULL);
+
 	return a->count;
 }
